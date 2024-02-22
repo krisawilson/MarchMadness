4e814514d2b252abe2cb7d5ea36ac1f45270e68a
Kristopher Wilson <kawilso7@ncsu.edu>
2024-02-21 22:10:06 -0500

    test message

--- data-cleaning.R
+++ data-cleaning.R
@@ -2,5 +2,13 @@
 suppressMessages(library(tidyverse))
 cbb <- read_csv("cbb.csv", na = c("", "NA", "N/A"))
 cbb <- na.omit(cbb)
-# read in 2003-2012
+# read in 2003-2012 from hoopR
 library(hoopR)
+
+old_data2 <- load_mbb_team_box(seasons = 2003:2013)
+playoffs <- old_data |> 
+  filter(season_type == 3) |> 
+  select(game_id:game_date, team_display_name, team_short_display_name,
+         team_home_away:turnovers, opponent_team_display_name,
+         opponent_team_short_display_name, opponent_team_score)
+rm(old_data)
