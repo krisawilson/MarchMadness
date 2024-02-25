ce83c0374091ef1cf767d65519c819e88efb13a3
Kristopher Wilson <kawilso7@ncsu.edu>
2024-02-24 23:35:10 -0500

    some edits

--- modeling.R
+++ modeling.R
@@ -2,10 +2,11 @@
 #setwd(C:\MarchMadness)
 suppressPackageStartupMessages(library(tidyverse))
 library(caret) # for parition and training setups
-library(themis) # for smote
+#require(themis) # used in smote
 library(glmnet) # for regularized regression
 library(xgboost) # xgboost
 library(rpart) # for decision tree
+#require(MASS) # used for polr. masks dplyr::select . gonna use caret tho
 
 # read in data
 df <- read_csv("data/clean-data.csv")
@@ -15,8 +16,17 @@ df <- df |> # add ordinal representation
     postseason == "R32" ~ 2, postseason == "S16" ~ 3,
     postseason == "E8" ~ 4, postseason == "F4" ~ 5,
     postseason == "2ND" ~ 6, postseason == "Champions" ~ 7,
-    TRUE ~ NA))
+    TRUE ~ NA),
+    postseason = as.factor(postseason))
 
-# set 1: not addressing response imbalance ----
+# set 1: preserve class distribution (imbalance) ----
+set.seed(1056)
+ctrl <- trainControl(method = "cv", number = 5) # set up CV
+ind <- createDataPartition(df$postseason, p = 0.75, list = F) # split data
+train_df <- df[ind,]
+test_df <- df[-ind,]
+
+# model 1: ordered logistic regression
 
 # set 2: addressing response imbalance ----
+ctrl2 <- trainControl(method = "cv", number = 5, sampling = "smote")
