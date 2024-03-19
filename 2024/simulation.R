6eef3a7851837af6c0d3a55dbd2863b29f661de5
Kristopher Wilson <kawilso7@ncsu.edu>
2024-03-19 17:33:24 -0400

    one more reorg

--- 2024/simulation.R
+++ 2024/simulation.R
@@ -1,49 +1,8 @@
-# packages----
+# packages and reading in----
 suppressMessages(library(tidyverse))
-library(ordinal)
-library(rvest)
-library(polite)
-# read in and clean data ----
-url <- "https://barttorvik.com/trank.php?year=2024&sort=&top=0&conlimit=All&venue=All&type=All#"
-# politely scrape
-nice_url <- bow(url = url) |> scrape()
-# actually get table
-tab_yr <- nice_url |> html_element(css = "#content > table") |> html_table()
-tnames <- tab_yr[1,] # extract first row as names
-tab_yr <- tab_yr[-1,] # remove first row
-colnames(tab_yr) <- tnames  # attach names
-rm(tnames, nice_url)
-
-clean_yr <- tab_yr |> 
-  filter(Rk != "Rk") |> # remove header rows from scraping
-  # create seed column
-  mutate(seed = str_extract(Team, "\\d+")) |> 
-  # split team into team and postseason finish
-  separate_wider_delim(cols = "Team", delim = ",", 
-                       names = c("Team", "postseason"),
-                       too_few = "align_start") |> 
-  # remove whitespace, remove seed, 
-  # remove comma, then remove whitespace again
-  mutate(Team = str_squish(Team),
-         Team = str_replace_all(Team, "\\d", ","),
-         Team = str_extract(Team, "^[^,]+"),
-         Team = str_squish(Team),
-         # remove hidden whitespace from postseason
-         postseason = str_squish(postseason)) |> 
-  # remove teams that didn't make tourney
-  na.omit(postseason) |> 
-  select(-c(Rk, G, Rec, postseason)) |> # keep wanted vars
-  janitor::clean_names() |> # clean names
-  # remove offensive 3 pt rate and defensive 3 pt rate defense
-  select(-x3pr, -x3prd) |> 
-  # change names to match cbb
-  rename("tor_d" = "tord", "ftr_d" = "ftrd",
-         "efg_o" = "efg_percent", "efg_d" = "efgd_percent",
-         "x2p_o" = "x2p_percent", "x2p_d" = "x2p_percent_d",
-         "x3p_o" = "x3p_percent", "x3p_d" = "x3p_percent_d") |> 
-  mutate(across(3:20, as.numeric))
-rm(tab_yr)
-
+library(ordinal) # for prediction
+# not sure why it's reading in weird, but removed the first column
+clean_yr <- read_csv("data/clean-2024.csv") |> select(-1)
 # predictions ----
 
 # load in model
@@ -55,6 +14,7 @@ std24 <- clean_yr |> select(-team, -conf, -seed) |> scale() |> data.frame()
 wholedf <- cbind(select(clean_yr, team, conf, seed), std24)
 # prediction!
 preds <- predict(probit, wholedf, type = "prob")
+p2 <- predict(probit, wholedf, type = "class")
 preds <- as.data.frame(preds) # turn into data frame 
 # change column names
 colnames(preds) <- sub("^fit\\.", "", colnames(preds))
