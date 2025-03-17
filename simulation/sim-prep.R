c0b102402970f21d6fafd0a2383a00e4d8a9507a
Kristopher Wilson <kawilso7@ncsu.edu>
2025-03-17 15:54:21 -0400

    fixed sim data

--- simulation/sim-prep.R
+++ simulation/sim-prep.R
@@ -1,3 +1,8 @@
+##################################################
+# THIS HAS TO BE DONE BEFORE THE FIRST FOUR GAMES
+# OTHERWISE THE NCAA BRACKET WILL CHANGE
+##################################################
+
 source("preprocessing/functions/data-cleaning-functions.R")
 source("simulation/functions/simulation-functions.R")
 
@@ -34,12 +39,31 @@ rm(url1, u25, rat25, ratings25, p25, bracket25, team_stats)
 full <- full_dat |> 
   mutate(across(3:length(full_dat), 
                 ~ifelse(is.na(.), min(., na.rm = TRUE), .))) |> 
-  select(-year)
+  select(-year) |> 
+  mutate(region = case_when(row_number() <= 16 ~ "SPOKANE1",
+                            row_number() <= 32 ~ "BIRMINGHAM1",
+                            row_number() <= 48 ~ "SPOKANE4",
+                            row_number() <= 64 ~ "BIRMINGHAM3"))
 
 rm(full_dat)
 
 # get all pairwise comparisons
 dat_for_sim <- pairwise_differences(df = full)
+
+# add region column
+dat_for_sim <- dat_for_sim |>
+  left_join(full |> 
+              select(region, team) |> 
+              rename(region1 = region, team1 = team), 
+            by = "team1",
+            relationship = "many-to-many") |>
+  left_join(full |> 
+              select(region, team) |> 
+              rename(region2 = region, team2 = team), 
+            by = "team2",
+            relationship = "many-to-many") |> 
+  relocate(c("region1", "region2"), .before = PPG_diff)
+
 # write data!
 readr::write_csv(dat_for_sim, "simulation/input-data.csv")
 
